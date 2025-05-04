6ca3420633810c6f326f8466bb6c65728e6ca2c3
choisujin96 <csjin9783@naver.com>
2025-05-05 02:09:39 +0900

    *5번 완*

--- src/com/javaex/ex05/Print.java
+++ src/com/javaex/ex05/Print.java
@@ -1,11 +1,26 @@
 package com.javaex.ex05;
 
 public class Print {
-    
+  
+	
+
     public void printer(int val){
         System.out.println(val);
     }
 
     //메소드  3개 를 작성하세요
+    
+    
+    public void printer(boolean tru) {
+    	System.out.println(tru);
+    }
+    
+    public void printer(double five) {
+    	System.out.println(five);
+    }
+    
+    public void printer(String hong) {
+    	System.out.println(hong);
+    }
     
 }
