3cdfab54c8d608e5fe538c8019e5060c24d045ad
choisujin96 <csjin9783@naver.com>
2025-05-06 15:49:28 +0900

    *7번도 완*

--- src/com/javaex/ex07/Friend.java
+++ src/com/javaex/ex07/Friend.java
@@ -3,16 +3,40 @@ package com.javaex.ex07;
 public class Friend {
 
     private String name;
-    private String hp;
+    private String hp;  //010은 숫자로 담지 못하고,
+    					//주민번호나 학번 같은 것도 문자열로 관리하는 게 더 좋음.
     private String school;
 
     
     // getter/setter 작성
+    public void setName(String name) {
+    	this.name=name;
+    }
     
+    public String getName() {
+    	return name;
+    }
+    
+    public void setHp(String hp) {
+    	this.hp=hp;
+    }
+    
+    public String getHp() {
+    	return hp;
+    }
+   
+    public void setSchool(String school) {
+    	this.school=school;
+    }
+    
+    public String getSchool() {
+    	return school;
+    }
     
     
     public void showInfo(){
-        System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
+        System.out.println("이름:" + name + "\t" + "핸드폰:" + hp +  
+        					"\t" + "학교:" + school);
     }
 
 }
--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -10,19 +10,38 @@ public class FriendApp {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("친구를 3명 등록해 주세요");
-
-        
+  
+         
         //for문 시작
         //친구정보 3명 입력 로직 --> 반복문 사용
-            
-      
+        for(int i=0; i<3; i++) {
+        	friendArray[i]= new Friend();
+        	
+        	
+        	System.out.print("이름:");
+        	String name=sc.next();
+        	friendArray[i].setName(name);
+        	
+        	System.out.print("핸드폰:");
+        	String hp=sc.next();
+        	friendArray[i].setHp(hp);
+        	
+        	System.out.print("학교:");
+        	String school=sc.next();
+        	friendArray[i].setSchool(school);
+        	
+        	System.out.println("-----------------");
+     
+        }
         //for문 끝
         
         
         // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
             //친구정보 출력 메소드 호출
+        	friendArray[i].showInfo();
         }
+       
 
         sc.close();
     }
